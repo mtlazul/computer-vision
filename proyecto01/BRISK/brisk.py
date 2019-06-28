9909d85aa50caa189afa8a9b52ffac67c68c62ce
fabisqc0207 <fabisqc0207@gmail.com>
2019-06-27 23:08:01 -0600

    Add BRISK implementation which uses video frames for matching

--- proyecto01/BRISK/brisk.py
+++ proyecto01/BRISK/brisk.py
@@ -6,78 +6,65 @@ import numpy as np
 import argparse
 from math import sqrt
 
-## [load]
-parser = argparse.ArgumentParser(description='Code for BRISK keypoint detector and descriptor extractor tutorial.')
-parser.add_argument('--input1', help='Path to input image 1.', default='graf1.png')
-parser.add_argument('--input2', help='Path to input image 2.', default='graf3.png')
-parser.add_argument('--homography', help='Path to the homography matrix.', default='H1to3p.xml')
-args = parser.parse_args()
 
-img1 = cv.imread(cv.samples.findFile(args.input1), cv.IMREAD_GRAYSCALE)
-img2 = cv.imread(cv.samples.findFile(args.input2), cv.IMREAD_GRAYSCALE)
-if img1 is None or img2 is None:
-    print('Could not open or find the images!')
-    exit(0)
+def BRISK(prev_frame, frame):
+	## [load]
+	gray0 = cv.cvtColor(prev_frame, cv.COLOR_BGR2GRAY)
+	gray1 = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+	## [load]
 
-fs = cv.FileStorage(cv.samples.findFile(args.homography), cv.FILE_STORAGE_READ)
-homography = fs.getFirstTopLevelNode().mat()
-## [load]
+	## [BRISK]
+	brisk = cv.BRISK_create()
+	kpts0, desc0 = brisk.detectAndCompute(gray0, None)
+	kpts1, desc1 = brisk.detectAndCompute(gray1, None)
+	## [BRISK]
 
-## [BRISK]
-brisk = cv.BRISK_create()
-kpts1, desc1 = brisk.detectAndCompute(img1, None)
-kpts2, desc2 = brisk.detectAndCompute(img2, None)
-## [BRISK]
+	## [Brute-Force matching]
+	# cv.NORM_HAMMING should be used for binary string based descriptor (e.g. ORB, BRISK)
+	# crossCheck ON for better results
+	matcher = cv.BFMatcher(cv.NORM_HAMMING, crossCheck=True)
+	matches = matcher.match(desc0, desc1)
+	matches = sorted(matches, key = lambda x:x.distance) # Sort them in the order of their distance
+	## [Brute-Force matching]
 
-## [2-nn matching]
-matcher = cv.DescriptorMatcher_create(cv.DescriptorMatcher_BRUTEFORCE_HAMMING)
-nn_matches = matcher.knnMatch(desc1, desc2, 2)
-## [2-nn matching]
+	## [draw final matches]
+	res = np.empty((max(gray0.shape[0], gray1.shape[0]), gray0.shape[1]+gray1.shape[1], 3), dtype=np.uint8)
+	cv.drawMatches(gray0, kpts0, gray1, kpts1, matches, res, flags=cv.DRAW_MATCHES_FLAGS_NOT_DRAW_SINGLE_POINTS)
+	## [draw final matches]
 
-## [ratio test filtering]
-matched1 = []
-matched2 = []
-nn_match_ratio = 0.8 # Nearest neighbor matching ratio
-for m, n in nn_matches:
-    if m.distance < nn_match_ratio * n.distance:
-        matched1.append(kpts1[m.queryIdx])
-        matched2.append(kpts2[m.trainIdx])
-## [ratio test filtering]
+	## [RESULTS]
+	print('\nBRISK Matching Results')
+	print('*******************************')
+	print('# Keypoints 1:                        \t', len(kpts0))
+	print('# Keypoints 2:                        \t', len(kpts1))
+	print('# Matches:                            \t', len(matches))
 
-## [homography check]
-inliers1 = []
-inliers2 = []
-good_matches = []
-inlier_threshold = 2.5 # Distance threshold to identify inliers with homography check
-for i, m in enumerate(matched1):
-    col = np.ones((3,1), dtype=np.float64)
-    col[0:2,0] = m.pt
+	return res
+	## [RESULTS]
 
-    col = np.dot(homography, col)
-    col /= col[2,0]
-    dist = sqrt(pow(col[0,0] - matched2[i].pt[0], 2) +\
-                pow(col[1,0] - matched2[i].pt[1], 2))
 
-    if dist < inlier_threshold:
-        good_matches.append(cv.DMatch(len(inliers1), len(inliers2), 0))
-        inliers1.append(matched1[i])
-        inliers2.append(matched2[i])
-## [homography check]
+def main():
+	cap = cv.VideoCapture(0)
+	if (cap.isOpened()== False):
+		print("Error opening camera stream")
+		cap.release()
+		return
 
-## [draw final matches]
-res = np.empty((max(img1.shape[0], img2.shape[0]), img1.shape[1]+img2.shape[1], 3), dtype=np.uint8)
-cv.drawMatches(img1, inliers1, img2, inliers2, good_matches, res)
-cv.imwrite("brisk_result.png", res)
+	ret, frame = cap.read()
+	while(cap.isOpened()):
+		prev_frame = frame[:]
+		ret, frame = cap.read()
+		if ret == True:
+			res = BRISK(prev_frame, frame)
+			cv.imshow(BRISK.__name__, res)
+			if cv.waitKey(25) & 0xFF == ord('q'):
+				break
+		else:
+			break
 
-inlier_ratio = len(inliers1) / float(len(matched1))
-print('BRISK Matching Results')
-print('*******************************')
-print('# Keypoints 1:                        \t', len(kpts1))
-print('# Keypoints 2:                        \t', len(kpts2))
-print('# Matches:                            \t', len(matched1))
-print('# Inliers:                            \t', len(inliers1))
-print('# Inliers Ratio:                      \t', inlier_ratio)
+	cap.release()
+	cv.destroyAllWindows()
 
-cv.imshow('result', res)
-cv.waitKey()
-## [draw final matches]
+
+if __name__ == "__main__":
+	main()
