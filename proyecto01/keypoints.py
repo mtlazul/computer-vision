ea73aeff2275a4ea9f7e6cc246f940bc9a67ced4
Miguel Taylor <miguel.taylor@ridgerun.com>
2019-06-28 22:21:40 -0600

    Add different options for algorithm inputs

--- proyecto01/keypoints.py
+++ proyecto01/keypoints.py
@@ -2,6 +2,7 @@
 import cv2 as cv
 import numpy as np
 import argparse
+import os
 from AKAZE import akaze
 from BRISK import brisk
 from KAZE import kaze
@@ -11,25 +12,88 @@ from SURF import surf
 
 def main():
 	if args.keypoints:
-		cap = cv.VideoCapture(0)
-		if (cap.isOpened()== False):
-			print("Error opening camera stream")
-			cap.release()
-			return
+		in1_image = False
+		in2_image = False
+		cap1 = None
+		cap2 = None
+		print (1)
 
-		ret, frame = cap.read()
-		while(cap.isOpened()):
-			prev_frame = frame[:]
-			ret, frame = cap.read()
-			if ret == True:
-				res = args.keypoints(prev_frame, frame)
-				cv.imshow(args.keypoints.__name__, res)
-				if cv.waitKey(25) & 0xFF == ord('q'):
-					break
+		root, ext = os.path.splitext(args.in1)
+		if ext in ['.bmp','.dib', '.jpeg', '.jpg', '.jpe', '.jp2', '.png', '.webp', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.tiff', '.tif']:
+			in1_image = True
+			frame1 = cv.imread(args.in1)
+			if frame1.size == 0:
+				print("\n\t\tError opening input 1 image\n")
+				if cap1 != None:
+					cap1.release()
+				if cap2 != None:
+					cap2.release()
+				return
+		else:
+			cap1 = cv.VideoCapture(args.in1)
+			ret, frame1 = cap1.read()
+			if (cap1.isOpened()== False):
+				print("\n\t\tError opening input 1 video\n")
+				if cap1 != None:
+					cap1.release()
+				if cap2 != None:
+					cap2.release()
+				return
+
+		if args.in1 == args.in2:
+			if not in1_image:
+				cap2 = cap1
+			frame2 = frame1
+		else:
+			root, ext = os.path.splitext(args.in2)
+			if ext in ['.bmp','.dib', '.jpeg', '.jpg', '.jpe', '.jp2', '.png', '.webp', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.tiff', '.tif']:
+				in2_image = True
+				frame2 = cv.imread(args.in2)
+				if frame2.size == 0:
+					print("\n\t\tError opening input 2 image\n")
+					if cap1 != None:
+						cap1.release()
+					if cap2 != None:
+						cap2.release()
+					return
+				print (4)
 			else:
+				cap2 = cv.VideoCapture(args.in2)
+				ret, frame2 = cap2.read()
+				if (cap2.isOpened()== False):
+					print("\n\t\tError opening input 2 video\n")
+					if cap1 != None:
+						cap1.release()
+					if cap2 != None:
+						cap2.release()
+					return
+				print (5)
+
+		while(1):
+			if not in1_image:
+				if not cap1.isOpened():
+					break
+				ret, frame1 = cap1.read()
+				if ret == False:
+					break
+			if not in2_image:
+				if not cap2.isOpened():
+					break
+				ret, frame2 = cap2.read()
+				if ret == False:
+					break
+
+			#frame2 = cv.resize(frame2, (frame1.shape[1],frame1.shape[0]), interpolation = cv.INTER_AREA)
+
+			res = args.keypoints(frame1, frame2)
+			cv.imshow(args.keypoints.__name__, res)
+			if cv.waitKey(25) & 0xFF == ord('q'):
 				break
 
-		cap.release()
+		if cap1 != None:
+			cap1.release()
+		if cap2 != None:
+			cap2.release()
 		cv.destroyAllWindows()
 	else:
 		print("\n\t\tPlease select a feature matching algorithm\n");
@@ -55,5 +119,11 @@ if __name__ == "__main__":
 	parser.add_argument('--surf', dest='keypoints', action='store_const',
 						const=surf.SURF, default=None,
 						help='Apply SURF algorithm for feature matching')
+	parser.add_argument('--in1', action="store", dest="in1",
+						default="/dev/video0", type=str,
+						help="First input of the feature matching algorithm. Can be a video device, image or video file")
+	parser.add_argument('--in2', action="store", dest="in2",
+						default="/dev/video0", type=str,
+						help="Second input of the feature matching algorithm.  Can be a video device, image or video file")
 	args = parser.parse_args()
 	main()
